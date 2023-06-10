202d73e5298e4ca8980cc59035b3f24aab5f59af
AvirukBasak <avirukbasak@gmail.com>
2023-06-10 23:40:51 +0530

    changed sky grad

--- src/util/ImageLoader.java
+++ src/util/ImageLoader.java
@@ -2,8 +2,9 @@ package util;
 
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+// import java.io.InputStream;
 
 public class ImageLoader
 {
@@ -18,9 +19,9 @@ public class ImageLoader
          */
         BufferedImage img = null;
         try {
-            InputStream ips = ImageLoader.class.getClassLoader().getResourceAsStream(path);
-            if (ips == null) throw new NullPointerException("input stream is null");
-            img = ImageIO.read(ips);
+            InputStream inputStream = ImageLoader.class.getClassLoader().getResourceAsStream(path);
+            if (inputStream == null) throw new NullPointerException("input stream is null");
+            img = ImageIO.read(inputStream);
             if (img == null) throw new NullPointerException("failed to load image");
         } catch(IOException ex) {
             ex.printStackTrace();
